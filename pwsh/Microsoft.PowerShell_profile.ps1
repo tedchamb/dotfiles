92e56c7c67ea002e86aa20a587842e07c07298ee
Ted Chambers <tedchamb@github.com>
2020-09-02 19:51:44 -0400

    fix Install-Module calls

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,9 @@
 if (-not (Get-Module -ListAvailable -Name posh-git)) {
-    Import-Module posh-git -Force -SkipPublisherCheck
+    Install-Module posh-git -Scope CurrentUser -Force -SkipPublisherCheck
 } 
 
 if (-not (Get-Module -ListAvailable -Name oh-my-posh)) {
-    Import-Module oh-my-posh -Force -SkipPublisherCheck
+    Install-Module oh-my-posh -Scope CurrentUser -Force -SkipPublisherCheck
 }
 
 if (-not (Get-Module -ListAvailable -Name PSReadLine)) {
