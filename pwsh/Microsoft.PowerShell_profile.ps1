2fd689f200b9fa1e649a1bb905f011ffbe4e6693
Ted Chambers <tedchamb@github.com>
2020-09-02 17:38:06 -0400

    pwsh profile

---
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,17 @@
+if (-not (Get-Module -ListAvailable -Name posh-git)) {
+    Import-Module posh-git -Scope CurrentUser -Force -SkipPublisherCheck
+} 
+
+if (-not (Get-Module -ListAvailable -Name oh-my-posh)) {
+    Import-Module oh-my-posh -Scope CurrentUser -Force -SkipPublisherCheck
+}
+
+if (-not (Get-Module -ListAvailable -Name PSReadLine)) {
+    Install-Module -Name PSReadLine -Scope CurrentUser -Force -SkipPublisherCheck
+}
+
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-Theme Paradox
+
+$env:AzureSubscriptionId = "8a53cb9a-a3a5-4602-aa2d-8c171edde3c7"
--- zsh/install.sh
+++ zsh/install.sh
@@ -6,7 +6,7 @@ then
   fi
 fi
 
-chsh -s $(which zsh)
+sudo chsh -s $(which zsh) $USER
 
 if [ ! -f "$HOME/.oh-my-zsh/oh-my-zsh.sh" ]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" ""  --unattended --keep-zshrc
