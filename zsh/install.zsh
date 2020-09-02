276745ba2c498bf72fb89811f378e868eb344d7a
Ted Chambers <tedchamb@github.com>
2020-09-02 00:47:04 -0400

    call install scripts

---
+++ zsh/install.zsh
@@ -0,0 +1,3 @@
+if [ ! -f "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+fi
