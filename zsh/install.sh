949748d43c2daf6d9dd6c52682b28754d386e494
Ted Chambers <tedchamb@github.com>
2020-09-02 00:56:21 -0400

    fix oh my zsh check

--- zsh/install.sh
+++ zsh/install.sh
@@ -6,6 +6,6 @@ then
   fi
 fi
 
-if [ ! -d "$HOME/.oh-my-zsh" ]; then
+if [ ! -f "$HOME/.oh-my-zsh/oh-my-zsh.sh" ]; then
   sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
