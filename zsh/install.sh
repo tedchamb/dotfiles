bc59d661e9201926782214a5f0f1af1c8dd16509
Ted Chambers <tedchamb@github.com>
2020-09-02 00:21:59 -0400

    install oh my Zsh and p10k

--- zsh/install.sh
+++ zsh/install.sh
@@ -5,3 +5,7 @@ then
     sudo apt install zsh
   fi
 fi
+
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
