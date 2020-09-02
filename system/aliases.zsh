c4be4a0c44cf273bb675470ab867faea6d738395
Ted Chambers <tedchamb@github.com>
2020-09-01 23:59:24 -0400

    ls aliases

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -3,8 +3,13 @@
 #   `brew install coreutils`
 if $(gls &>/dev/null)
 then
-  alias ls="gls -F --color"
-  alias l="gls -lAh --color"
-  alias ll="gls -l --color"
+  alias ls='gls -F --color'
+  alias l='gls -lAh --color'
+  alias ll='gls -l --color'
   alias la='gls -A --color'
+else
+  alias ls='ls --color=auto'
+  alias l='ls --color=auto -lAh'
+  alias ll='ls --color=auto -l'
+  alias la='ls --color=auto -A'
 fi
