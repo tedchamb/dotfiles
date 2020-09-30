2b301c8aca2d0a2b3794f9231f4142be66eebdf6
Ted Chambers <tedchamb@Teds-MacBook-Pro.local>
2020-09-30 19:45:13 -0400

    update powerlevel10k url

--- p10k/install.zsh
+++ p10k/install.zsh
@@ -1,3 +1,3 @@
 if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+  git clone --depth=1 git://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 fi
