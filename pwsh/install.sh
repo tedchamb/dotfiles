ee66570863b8f45092388ad8aaa7a7e010981ce5
Ted Chambers <tedchamb@github.com>
2022-07-24 18:41:38 -0400

    fix simlink

--- pwsh/install.sh
+++ pwsh/install.sh
@@ -2,5 +2,5 @@
 
 if [ ! -e "$HOME/.config/powershell/Microsoft.PowerShell_profile.ps1" ]; then
   mkdir "$HOME/.config/powershell" --parent
-  link_file "$DOTFILES_ROOT/pwsh/Microsoft.PowerShell_profile.ps1" "$HOME/.config/powershell/Microsoft.PowerShell_profile.ps1"
+  ln -s "$DOTFILES_ROOT/pwsh/Microsoft.PowerShell_profile.ps1" "$HOME/.config/powershell/Microsoft.PowerShell_profile.ps1"
 fi
