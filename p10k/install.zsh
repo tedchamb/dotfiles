014fce2e3222e4c1c2dca67fd3928a18acf811b6
Ted Chambers <tedchamb@github.com>
2022-07-24 19:06:02 -0400

    p10k

--- p10k/install.zsh
+++ p10k/install.zsh
@@ -4,4 +4,7 @@ if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
   git clone --depth=1 git://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 fi
 
-zsh -c "$DOTFILESPATH/p10k/p10k.symlink"
+if [ ! -a "$HOME/.p10k.zsh" ]; then
+  mkdir "$HOME/.config/powershell" --parent
+  ln -s "$DOTFILESPATH/p10k/p10k.zsh.symlink" "$HOME/.p10k.zsh"
+fi
