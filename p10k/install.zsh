d229292d40618a6db9aa311eb8b23d61f543b05a
Ted Chambers <tedchamb@github.com>
2022-07-25 10:15:00 -0400

    double bracket

--- p10k/install.zsh
+++ p10k/install.zsh
@@ -4,7 +4,7 @@ if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
   git clone --depth=1 https://github.com/romkatv/powerlevel10k ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 fi
 
-if [ ! -L "$HOME/.p10k.zsh" && ! -e "$HOME/.p10k.zsh" ]; then
+if [[ ! -L "$HOME/.p10k.zsh" && ! -e "$HOME/.p10k.zsh" ]]; then
   mkdir "$HOME/.config/powershell" --parent
   ln -s "$DOTFILESPATH/p10k/p10k.zsh.symlink" "$HOME/.p10k.zsh"
 fi
