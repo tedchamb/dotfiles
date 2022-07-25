d9e9b32861c8c0ac01a7eb4ebf8ff22119994f06
Ted Chambers <tedchamb@github.com>
2022-07-25 09:58:27 -0400

    fix var

--- pwsh/install.sh
+++ pwsh/install.sh
@@ -2,5 +2,5 @@
 
 if [[ ! -e "$HOME/.config/powershell/Microsoft.PowerShell_profile.ps1" ]]; then
   mkdir "$HOME/.config/powershell" --parent
-  ln -s "$DOTFILES_ROOT/pwsh/Microsoft.PowerShell_profile.ps1" "$HOME/.config/powershell/Microsoft.PowerShell_profile.ps1"
+  ln -s "$DOTFILESPATH/pwsh/Microsoft.PowerShell_profile.ps1" "$HOME/.config/powershell/Microsoft.PowerShell_profile.ps1"
 fi
