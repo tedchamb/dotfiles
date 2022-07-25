c683dade3c30646ebfb452a594676b4dea2d4e4b
Ted Chambers <tedchamb@github.com>
2022-07-25 09:35:06 -0400

    fix powerlevel10k clone

--- p10k/install.zsh
+++ p10k/install.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
-  git clone --depth=1 git://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 fi
 
 if [ ! -a "$HOME/.p10k.zsh" ]; then
