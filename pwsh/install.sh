df3e77877810e9e3814f7f28d574f47ba0b5f373
Ted Chambers <tedchamb@github.com>
2022-07-25 09:52:07 -0400

    switch to -a

--- pwsh/install.sh
+++ pwsh/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ ! -e "$HOME/.config/powershell/Microsoft.PowerShell_profile.ps1" ]; then
+if [ ! -a "$HOME/.config/powershell/Microsoft.PowerShell_profile.ps1" ]; then
   mkdir "$HOME/.config/powershell" --parent
   ln -s "$DOTFILES_ROOT/pwsh/Microsoft.PowerShell_profile.ps1" "$HOME/.config/powershell/Microsoft.PowerShell_profile.ps1"
 fi
