130c0f0137ba7c301f1e889e8c9626731d1b0221
Ted Chambers <tedchamb@github.com>
2022-12-04 17:24:10 -0500

    pwsh macros

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -4,7 +4,9 @@ $env:AzureSubscriptionId = '8a53cb9a-a3a5-4602-aa2d-8c171edde3c7'
 
 if ($env:CODESPACES -eq 'true') {
     # Start-Actions
-    function global:sa { zsh -c "unset SKYRISEV3 && $env:GITHUB_PATH/script/actions/start-actions" }
+    function global:sa {
+        zsh -c "unset SKYRISEV3 && $env:GITHUB_PATH/script/actions/start-actions"
+    }
 
     # New Codespace Setup
     function global:ncs {
@@ -16,22 +18,35 @@ if ($env:CODESPACES -eq 'true') {
         zsh -c "cd '$env:LAUNCH_PATH' && script/setup --force"
         sa
     }
-}
 
-if ($env:SKYRISEV3 -eq 'true') {
-    # Build Run L0s
-    function global:brl0 {
-        b
-        if ($LastExitCode -eq 0) {
-            l0
-        }
+    # runner job agent logs
+    function global:rjl {
+        kubectl logs $(kubectl get pods -o name | grep 'pod/runner-job-agent-') --follow
     }
 
-    # Build Run L1s
-    function global:brl1 {
-        b
-        if ($LastExitCode -eq 0) {
-            l1
-        }
+    # runner api server logs
+    function global:ral {
+        kubectl logs $(kubectl get pods -o name | grep 'pod/runner-api-server-') --follow
+    }
+}
+
+# kubectl
+function global:k {
+    kubectl $args
+}
+
+# Build Run L0s
+function global:brl0 {
+    b
+    if ($LastExitCode -eq 0) {
+        l0
+    }
+}
+
+# Build Run L1s
+function global:brl1 {
+    b
+    if ($LastExitCode -eq 0) {
+        l1
     }
 }
