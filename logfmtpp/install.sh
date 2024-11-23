27c3e9f8db4e21175a805b52b9c75ebfb7858dba
Ted Chambers <tedchamb@github.com>
2024-11-23 11:01:24 -0500

    tweaks

--- logfmtpp/install.sh
+++ logfmtpp/install.sh
@@ -4,18 +4,21 @@ set -e
 
 scriptDir=$(dirname "$0")
 
-rm -dr "$scriptDir/bin"
+binDir = "$scriptDir/bin"
+if [[ -d "$dir_to_remove" ]]; then
+    rm -dr "$binDir"
+fi
 
-$scriptDir/../github/download-release.ps1 -Repo tedchamb/logfmtpp -Name logfmtpp -Destination "$scriptDir/bin"
+$scriptDir/../github/download-release.ps1 -Repo tedchamb/logfmtpp -Name logfmtpp -Destination "$binDir"
 
-chmod +x "$scriptDir/bin/logfmtpp"
+$toolPath = "$binDir/logfmtpp"
+chmod +x "$toolPath"
 
-tool="$scriptDir/bin/logfmtpp"
-symlinkPath="/usr/local/bin/$(basename "$tool")"
+symlinkPath="/usr/local/bin/$(basename "$toolPath")"
 
 if [[ ! -L "$symlinkPath" ]]; then
-    echo "Adding symlink to $tool at $symlinkPath"
-    sudo ln -s "$(realpath "$tool")" "$symlinkPath"
+    echo "Adding symlink to $toolPath at $symlinkPath"
+    sudo ln -s "$(realpath "$toolPath")" "$symlinkPath"
 else
     echo "Path for symlink already exists: $symlinkPath"
 fi
